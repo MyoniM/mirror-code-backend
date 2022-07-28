6050d8271b08df79ba1b1ce781f279950ceac0ae
MyoniM <se.yonatan.merkebu@gmail.com>
2022-07-28 17:15:20 +0300

    refactored for socket, socket back-bone

--- core/executePython.js
+++ core/executePython.js
@@ -3,9 +3,9 @@ const { exec } = require("child_process");
 const executePython = (filePath) => {
   return new Promise((resolve, reject) => {
     exec(`python ${filePath}`, (error, stdout, stderr) => {
-      stderr && resolve(stderr);
+      stderr && resolve({ data: stderr, stderr: true });
       error && reject({ error, stderr });
-      resolve(stdout);
+      resolve({ data: stdout, stderr: false });
     });
   });
 };
--- index.js
+++ index.js
@@ -1,33 +1,58 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
-const rateLimit = require("express-rate-limit");
-var morgan = require("morgan");
-var fs = require("fs");
-var path = require("path");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+
+// local imports
+const { userJoined, leaveRoom, getCurrentUser, getRoomUsers } = require("./utils/users");
+const { formatMessage } = require("./utils/messages");
 
 const app = express();
-
 // ? create a write stream (in append mode)
 var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
   flags: "a",
 });
 
-// // ? create a api rate limiter for email verification
-// const emailAPIRateLimiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 5,
-// });
-
-app.use(cors());
 app.use(express.json());
 app.use(morgan("combined", { stream: accessLogStream }));
 
-// routes
-app.use("/api/v1", require("./routes/code.route"));
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+const serverName = "Mirror-Code";
+// runs when a new client connects
+io.on("connection", (socket) => {
+  socket.on("join_room", ({ username, room }) => {
+    if (!username) return;
+    const user = userJoined(socket.id, username, room);
+    socket.join(user.room);
+    // notify the user that just joined
+    socket.emit("message", formatMessage(serverName, "You joined a room."));
+    // notify every user in the room except the user that just joined
+    socket.broadcast.to(user.room).emit("message", formatMessage(serverName, `${user.username} joined the room.`));
+    // Send users and room info
+    io.to(user.room).emit("room_users", { room: user.room, users: getRoomUsers(user.room) });
+  });
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    const user = leaveRoom(socket.id);
+    if (user) {
+      socket.leave();
+      io.to(user.room).emit("message", formatMessage(serverName, `${user.username} has left the room`));
+      // Send users and room info
+      io.to(user.room).emit("room_users", { room: user.room, users: getRoomUsers(user.room) });
+    }
+  });
+});
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });

3053be7d7571ad27fb0f1e98d992abe860391feb
MyoniM <se.yonatan.merkebu@gmail.com>
2022-07-31 19:05:10 +0300

    updated cors policy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://mirror-code.web.app/",
   },
 });
 
